ca2776ba18ed58b591293f6f32227bea4405e325
Robert Heath <robert.b.heathii@gmail.com>
2021-01-15 11:11:32 -0600

    Write tests for user story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Hospital.destroy_all
+Doctor.destroy_all
+Surgery.destroy_all
+DoctorSurgery.destroy_all
+
+hospital = Hospital.create!(name: "Divine Hospital")
+
+doctor = Doctor.create!(name: "Justin Forsyth", specialty: "gastroenterology", hospital_id: hospital.id)
+doctor2 = Doctor.create!(name: "House, MD", specialty: "dermatology", hospital_id: hospital.id)
+doctor3 = Doctor.create!(name: "Sarah Heath", specialty: "ocular degeneration", hospital_id: hospital.id)
+
+appendectomy = Surgery.create!(Title: 'ppendectomy', day_of_the_week: "Monday", room_number: 10)
+lobotomy = Surgery.create!(Title: 'lobotomy', day_of_the_week: "Tuesday", room_number: 12)
+wart = Surgery.create!(Title: 'wart', day_of_the_week: 'Wednesday', room_number: 13)
+cataracts = Surgery.create!(Title: 'cataracts', day_of_the_week: 'Thursday', room_number: 15)
+
+DoctorSurgery.create!(doctor: doctor, surgery: appendectomy)
+DoctorSurgery.create!(doctor: doctor, surgery: lobotomy)
+DoctorSurgery.create!(doctor: doctor2, surgery: wart)
+DoctorSurgery.create!(doctor: doctor3, surgery: cataracts)
+DoctorSurgery.create!(doctor: doctor2, surgery: appendectomy)
---
+++ spec/features/surgeries/index_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe 'surgery index page' do
+  before :each do
+    @hospital = Hospital.create!(name: "Divine Hospital")
+
+    @doctor = Doctor.create!(name: "Justin Forsyth", specialty: "gastroenterology", hospital_id: @hospital.id, graduated_from: "Harvard")
+    @doctor2 = Doctor.create!(name: "House, MD", specialty: "dermatology", hospital_id: @hospital.id, graduated_from: "Yale")
+    @doctor3 = Doctor.create!(name: "Sarah Heath", specialty: "ocular degeneration", hospital_id: @hospital.id, graduated_from: "Stanford")
+
+    @appendectomy = Surgery.create!(title: 'appendectomy', day_of_the_week: "Monday", room_number: 10)
+    @lobotomy = Surgery.create!(title: 'lobotomy', day_of_the_week: "Tuesday", room_number: 12)
+    @wart = Surgery.create!(title: 'wart', day_of_the_week: 'Wednesday', room_number: 13)
+    @cataracts = Surgery.create!(title: 'cataracts', day_of_the_week: 'Thursday', room_number: 15)
+
+    DoctorSurgery.create!(doctor: @doctor, surgery: @appendectomy)
+    DoctorSurgery.create!(doctor: @doctor, surgery: @lobotomy)
+    DoctorSurgery.create!(doctor: @doctor2, surgery: @wart)
+    DoctorSurgery.create!(doctor: @doctor3, surgery: @cataracts)
+    DoctorSurgery.create!(doctor: @doctor2, surgery: @appendectomy)
+  end
+
+  describe 'when I visit the index page' do
+    it 'shows the titles of all surgeries' do
+      visit '/surgeries'
+
+      expect(current_path).to eq("/surgeries")
+
+      within "#surgery-#{@appendectomy.id}" do
+        expect(page).to have_content(@appendectomy.title)
+        expect(page).to have_content(@doctor.name)
+        expect(page).to have_content(@doctor2.name)
+      end
+
+      within "#surgery-#{@lobotomy.id}" do
+        expect(page).to have_content(@lobotomy.title)
+        expect(page).to have_content(@doctor.name)
+      end
+
+      within "#surgery-#{@wart.id}" do
+        expect(page).to have_content(@wart.title)
+        expect(page).to have_content(@doctor2.name)
+      end
+
+      within "#surgery-#{@cataracts.id}" do
+        expect(page).to have_content(@cataracts.title)
+        expect(page).to have_content(@doctor3.name)
+      end
+    end
+
+    it 'shows all the doctors performing those surgeries' do
+
+    end
+  end
+
+end
